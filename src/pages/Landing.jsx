63efbb16a4a294493cd7feb6c9c3431c5b58f7a0
mate444 <mateoeo23@gmail.com>
2024-02-16 18:50:25 +0100

    Agrega componente Carousels

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,13 @@
 import IntroSection from '../components/Landing/IntroSection';
+import Highlights from '../components/Landing/Highlights';
+import Carousels from '../components/Landing/Carousels/Carousels';
 
 const Landing = () => {
   return (
     <>
       <IntroSection />
+      <Highlights />
+      <Carousels />
     </>
   );
 };
