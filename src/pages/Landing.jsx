7a846f356a327f64ec1293719e6e0ece6b9caa50
mate444 <mateoeo23@gmail.com>
2024-02-20 13:31:28 +0100

    Refactoriza pagina Landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
-import IntroSection from '../components/Landing/IntroSection';
-import Highlights from '../components/Landing/Highlights';
-import Carousels from '../components/Landing/Carousels/Carousels';
+import IntroSection from '../components/Landing/IntroSection/IntroSection';
+import Highlights from '../components/Landing/HighlightsSection/HighlightsSection';
+import Carousels from '../components/Landing/CarouselSection/CarouselSection';
 
 const Landing = () => {
   return (
