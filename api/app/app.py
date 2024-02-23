d961f39360a7eab694f8577ff25dfab8f4fa163f
Mateo Hernandez <73136132+mate444@users.noreply.github.com>
2024-02-23 18:46:28 +0100

    Revert "Feat/front/home"

--- api/app/app.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from controllers import project
-
-app = FastAPI()
-
-app.include_router(project.api)
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Landing from './pages/Landing';
-import Home from './pages/Home';
 import NavBar from './components/NavBar/NavBar';
-import ProjectDetail from './pages/ProjectDetail';
-import './App.css';
 
 function App() {
   return (
@@ -11,8 +8,6 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Landing />}/>
-        <Route path="/detail/" element={<ProjectDetail />}/>
-        <Route path='/home' element={<Home />}/>
       </Routes>
     </>
   );
