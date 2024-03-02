bfd8b288ec3ade013da11f5df334c6eed908e23a
mate444 <mateoeo23@gmail.com>
2024-03-02 18:44:29 +0100

    Agrega hook useSortProjects a ProjectsContext

--- client/src/context/ProjectsContext.jsx
+++ client/src/context/ProjectsContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from 'react';
-import { LANGUAGES, COLLECTION, COMPLETE, INCOMPLETE } from '../utils/constants';
+import { BACKERS, EXPIRATION, FINANCED, NEWEST, LANGUAGES, COLLECTION, COMPLETE, INCOMPLETE } from '../utils/constants';
 import data from '../utils/projects.json';
 
 const ProjectsContext = createContext(null);
@@ -36,6 +36,57 @@ export const useFilterProjects = () => {
       newProjects = newProjects.filter((p) => p.progress < 100);
     } else setProjects(data);
     setProjects(newProjects);
+  };
+};
+
+export const useSortProjects = () => {
+  const { setProjects } = useContext(ProjectsContext);
+  return function(sort) {
+    setProjects((oldState) => {
+      const newProjects = [...oldState];
+      switch (sort) {
+      case NEWEST: {
+        newProjects.sort((a, b) => {
+          const firstDate = new Date(a.createdAt).getTime();
+          const secondDate = new Date(b.createdAt).getTime();
+          if (firstDate > secondDate) return 1;
+          else return -1;
+        });
+        break;
+      }
+      case FINANCED: {
+        newProjects.sort((a, b) => {
+          return b.amountCollected - a.amountCollected;
+        });
+        break;
+      }
+      case BACKERS: {
+        newProjects.sort((a, b) => {
+          if (a.backers < b.backers) return 1;
+          else return -1;
+        });
+        break;
+      }
+      case EXPIRATION: {
+        newProjects.sort((a, b) => {
+          const firstDate = new Date(a.createdAt).getTime();
+          const secondDate = new Date(b.createdAt).getTime();
+          if (firstDate < secondDate) return 1;
+          else return -1;
+        });
+        break;
+      }
+      default: return oldState;
+      }
+      return newProjects;
+    });
+  };
+};
+
+export const useSearchProjects = () => {
+  const { setProjects } = useContext(ProjectsContext);
+  return function(search) {
+    setProjects(data.filter((p) => p.title.toLowerCase().includes(search.toLowerCase().trim())));
   };
 };
 
