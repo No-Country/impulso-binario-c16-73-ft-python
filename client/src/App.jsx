23bd3d5df09c706026ac2dc72924977cb04b28f3
mate444 <mateoeo23@gmail.com>
2024-03-03 22:24:12 +0100

    Integra PaymentProvider a Rewards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,30 @@
 import { Route, Routes } from 'react-router-dom';
-import Landing from './pages/Landing';
+import { ProjectProvider } from './context/ProjectContext';
+import { ProjectsProvider } from './context/ProjectsContext';
+import { PaymentProvider } from './context/PaymentContext';
 import Home from './pages/Home';
+import Landing from './pages/Landing';
 import NavBar from './components/NavBar/NavBar';
 import ProjectDetail from './pages/ProjectDetail';
 import Rewards from './pages/Rewards';
-import { ProjectProvider } from './context/ProjectContext';
-import { ProjectsProvider } from './context/ProjectsContext';
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Landing />}/>
-        <Route path='/home' element={<ProjectsProvider><Home /></ProjectsProvider>}/>
-        <Route path="/detail/:id" element={<ProjectProvider><ProjectDetail /></ProjectProvider>}/>
-        <Route path='/project/:id/rewards' element={<ProjectProvider><Rewards /></ProjectProvider>}/>
+        <Route
+          path="/"
+          element={<Landing />}/>
+        <Route
+          path='/home'
+          element={<ProjectsProvider><Home /></ProjectsProvider>}/>
+        <Route
+          path="/detail/:id"
+          element={<ProjectProvider><ProjectDetail /></ProjectProvider>}/>
+        <Route
+          path='/project/:id/rewards'
+          element={<PaymentProvider><ProjectProvider><Rewards /></ProjectProvider></PaymentProvider>}/>
       </Routes>
     </>
   );
--- client/src/components/Rewards/RewardList/RewardList.jsx
+++ client/src/components/Rewards/RewardList/RewardList.jsx
@@ -1,28 +1,41 @@
+import { usePayment } from '../../../context/PaymentContext';
 import { useProject } from '../../../context/ProjectContext';
-import { Box, Flex, Heading, LinkBox, LinkOverlay } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Flex, Heading } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import CustomContribution from '../CustomContribution/CustomContribution';
 import Reward from '../Reward/Reward';
 
 const RewardList = () => {
   const { project: project } = useProject();
+  const navigate = useNavigate();
+  const { setPaymentInfo } = usePayment();
+  const handleSelection = (reward, price) => {
+    if (!reward) {
+      setPaymentInfo({ project: {
+        title: project.title,
+        progress: project.progress,
+        user: project.user,
+        mainImage: project.mainImage,
+      }, rewards: [] }, price);
+    } else {
+      setPaymentInfo({
+        project: {
+          title: project.title,
+          progress: project.progress,
+          user: project.user,
+          mainImage: project.mainImage,
+        },
+        rewards: [reward],
+      }, price);
+    }
+    navigate(`/project/${project.id}/checkout`);
+  };
   return (
     <Flex color={'#333333'} flexDir={'column'} gap={8} px={'80px'}>
       <Heading>
         Selecciona tu recompensa
       </Heading>
-      <LinkBox>
-        <Box
-          p={'20px'}
-          borderRadius={30}
-          border={'1px'}
-          borderColor={'#333333'}>
-          <Heading size={'lg'}>
-            <LinkOverlay as={Link} to='/project/:id/checkout'>
-            Contribuir sin recompensa
-            </LinkOverlay>
-          </Heading>
-        </Box>
-      </LinkBox>
+      <CustomContribution handleSelection={handleSelection}/>
       <Flex>
         {
           project.rewards?.length > 0 && project.rewards.map((r) => (
@@ -33,7 +46,8 @@ const RewardList = () => {
               image={r.image}
               price={r.price}
               projectId={project.id}
-              title={r.title}/>
+              title={r.title}
+              handleSelection={handleSelection}/>
           ))
         }
       </Flex>
